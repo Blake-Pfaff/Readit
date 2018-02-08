ea4463a9c9f15dbd7b52e9ff3edb60a9c4448edf
Blake Pfaff <blake.a.pfaff2@gmail.com>
2018-02-07 20:03:09 -0600

    fix conflects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
-<<<<<<< HEAD
 
     private
 
@@ -19,6 +18,4 @@ class ApplicationController < ActionController::Base
         redirect_to new_session_path
       end
     end
-=======
->>>>>>> 88a740581ec58727348298b674faa22a7990f9aa
 end
